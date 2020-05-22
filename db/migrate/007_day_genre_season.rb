4764b280385eefe69ca53a4440af7a87f5f25023
Jamal Rjoul <jamensky1991@gmail.com>
2020-05-22 09:49:35 +0000

    Automatically backed up by Learn

--- db/migrate/007_day_genre_season.rb
+++ db/migrate/007_day_genre_season.rb
@@ -1,7 +1,9 @@
 class AddDayGenreSeasonToShow < ActiveRecord::Migration[5.1]
 
   def change
-    add
+    add_column :shows, :day, :string
+    add_column :shows, :genre, :string
+    add_column :shows, :season, :string 
   end
 
 end
