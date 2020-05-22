4aa2b6410565123d96825283fff30d62e8ab86f9
Jamal Rjoul <jamensky1991@gmail.com>
2020-05-22 09:40:01 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,9 +4,7 @@ class Show < ActiveRecord::Base
   belongs_to :network
 
   def actors_list
-    self.characters.collect do |c|
-      "#{c.actor.first_name} #{c.actor.last_name}"
-      binding.pry
+    self.characters.collect{|c| "#{c.actor.first_name} #{c.actor.last_name}"}
     end
   end
 
